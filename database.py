7d9f877c9d3eabd1e4645198753c0170e798a326
new053502-wq <new053502@gmail.com>
2026-05-02 09:18:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,21 @@
+import sqlite3
 
+DB_PATH = "bot.db"
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    return conn
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS groups (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        chat_id TEXT UNIQUE
+    )
+    """)
+
+    conn.commit()
+    conn.close()
